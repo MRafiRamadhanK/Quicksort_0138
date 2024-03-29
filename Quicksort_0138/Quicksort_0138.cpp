3dbd63f28a270fe3782d1648a14a1d62f0d929e0
MRafiramadhanK <raficool271003@gmail.com>
2024-03-29 09:28:22 +0700

    void input pt 2

--- Quicksort_0138/Quicksort_0138.cpp
+++ Quicksort_0138/Quicksort_0138.cpp
@@ -12,6 +12,18 @@ void input()
 		cout << "masukan panjang element array : ";
 		cin >> n;
 
-		
+		if (n < +20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+	cout << "\n----------------" << endl;
+	cout << "\nEnter array element" << endl;
+	cout << "\n----------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> [i];
 	}
 }
